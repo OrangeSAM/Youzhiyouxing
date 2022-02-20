8079786bd67d0288e2baf44e60b19d8d15ea97e3
SamLiu <amazing_sam@outlook.com>
2022-02-20 21:54:45 +0800

    feat: ✅实现内容的展现

--- content.js
+++ content.js
@@ -1,19 +1,61 @@
-
 // 图标区模板
 const iconTemplate = `
-  <div>
-    <img src="./img/geren.png" alt="个人">
-  </div>
-  <div>
-    <img src="./img/remen.png" alt="热门">
-  </div>
+<div>
+  <img id="personal" src="./img/geren.png" alt="个人">
+</div>
+<div>
+  <img id="hot" src="./img/remen.png" alt="热门">
+</div>
 `
-function createIcon() {
-  const iconWrap = document.createElement('div')
 
+const panelTemplate = `
+  <div>
+    
+  </div>  
+`
+
+function init () {
+  const url = window.location.href
+  if (url.indexOf('materials')) {
+    createIcon()
+  }
+}
+
+function createIcon () {
+  const iconWrap = document.createElement('div')
   iconWrap.innerHTML = iconTemplate
   iconWrap.classList.add('icon-wrap')
   document.body.appendChild(iconWrap)
 }
 
-createIcon()
+const ul = document.createElement('ul')
+
+function createPanel (sentence) {
+  let panelWrap = document.createElement('div')
+  createHotContent(sentence)
+  createCopyBtn()
+  panelWrap.appendChild(ul)
+  panelWrap.classList.add('panel-wrap')
+  document.body.appendChild(panelWrap)
+}
+
+function createHotContent(sentence) {
+  for (let i = 0; i < sentence.length; i++) {
+    const li =document.createElement('li')
+    li.innerText = sentence[i].innerText
+    li.classList.add('single-sentence')
+    ul.appendChild(li)
+  }
+}
+
+function createCopyBtn(){
+  
+}
+
+init()
+
+personal.addEventListener('click', function () {
+  const hotSentence = document.querySelectorAll('.zx-rangy-hot')
+  createPanel(hotSentence)
+  console.log(hotSentence)
+})
